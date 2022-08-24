78a365d7f075325a2244737d8b35ea017b3668cf
nishikawa-r <102705383+nishikawa-r@users.noreply.github.com>
2022-08-24 13:46:56 +0900

    Add files via upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class おじょじょじょじょぼじゅぼぼ
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 19);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ユーザー名";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(100, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 19);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "パスワード";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(100, 114);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(120, 19);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ディレクトリ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(100, 159);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(246, 19);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(352, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(226, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "表示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 55);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(193, 203);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 55);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "実行";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Location = new System.Drawing.Point(30, 277);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(731, 161);
+            this.textBox1.TabIndex = 13;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // おじょじょじょじょぼじゅぼぼ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Name = "おじょじょじょじょぼじゅぼぼ";
+            this.Text = "おじょじょじょじょぼじゅぼぼ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class おじょじょじょじょぼじゅぼぼ : Form
+
+    {
+        private string Direct { get; set; }
+        private string Arguments { get; set; }
+        private FolderBrowserDialog Fbd { get; set; }
+        private Properties.Settings Default { get; set; }
+        private ComandProcess comandprocess { get; set; }
+        private bool Flag { get; set; }
+        private string output { get; set; }
+        CancellationTokenSource tokenSource;
+        CancellationToken cancelToken;
+
+        public おじょじょじょじょぼじゅぼぼ()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            Fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            Fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            Fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            Fbd.SelectedPath = @"C:\Windows";
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            Fbd.ShowNewFolderButton = true;
+
+            //ダイアログを表示する
+            if (Fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                Console.WriteLine(Fbd.SelectedPath);
+                textBox5.Text = Fbd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "表示")
+            {
+                button2.Text = "非表示";
+                // システムのパスワード文字を設定
+                textBox4.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                // システムのパスワード文字を設定
+                textBox4.UseSystemPasswordChar = true;
+                button2.Text = "表示";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Default.password = textBox4.Text;
+            Default.username = textBox3.Text;
+            Default.url = textBox2.Text;
+            Default.directory = textBox5.Text;
+            Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Default = Properties.Settings.Default;
+            textBox2.Text = Default.url;
+            textBox3.Text = Default.username;
+            textBox4.Text = Default.password;
+            textBox5.Text = Default.directory;
+
+        }
+
+        private async  void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "実行")
+            {
+                Controller Controller = new Controller();
+                Controller.Direct = textBox5.Text;
+                Controller.Url = textBox2.Text;
+                Controller.Username = textBox3.Text;
+                Controller.Password = textBox4.Text;
+                comandprocess = new ComandProcess();
+                Arguments = comandprocess.CreateArguments(Controller);
+                button4.Text = "実行中";
+                tokenSource = new CancellationTokenSource();
+                cancelToken = tokenSource.Token;
+                backgroundWorker1.RunWorkerAsync();
+
+            }
+            else
+            {
+
+                Flag = false;
+                backgroundWorker1.CancelAsync();
+                await ButtonUp();
+
+
+
+            }
+        }
+        private Task<string> ButtonUp (){
+            Task<string> task = Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (Flag)
+                    {
+                        InvokeButton();
+                        break;
+                    }
+                }
+                return "Stop";
+            });
+
+            return task;       
+}
+
+        private  async void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            Direct = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+            Direct = Direct + @"\echo.bat";
+            string command = Direct;
+
+            string arguments = Arguments;
+            Console.WriteLine(arguments);
+            ProcessStartInfo p = new ProcessStartInfo();
+            p.Arguments = arguments;
+            p.CreateNoWindow = true; // コンソールを開かない
+            p.UseShellExecute = false; // シェル機能を使用しない
+            p.FileName = command;
+            BackgroundWorker worker = (BackgroundWorker)sender;
+            p.RedirectStandardOutput = true; // 標準出力をリダイレクト
+            Process PsInfo = Process.Start(p);
+            output = "";
+            Invoke(output);
+            Task<string> task;
+            while (!PsInfo.HasExited)
+            {
+                 task =Task.Run(async() =>
+                {
+                    return await Ho(PsInfo);
+                });
+
+
+
+
+                worker = (BackgroundWorker)sender;
+
+                //キャンセル判定
+                // senderの値はbgWorkerの値と同じ
+
+                Console.WriteLine(worker.CancellationPending);
+                // 時間のかかる処理
+                // キャンセルされてないか定期的にチェック
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    InvokeButton2();
+
+                    Stop(PsInfo); 
+                    break;
+
+                }
+
+
+            }
+
+            //PsInfo.Dispose();
+            Console.WriteLine(worker.CancellationPending);
+
+            Flag = worker.CancellationPending;
+
+            //form1.Text += "end!";
+
+        }
+        public async Task<string> Ho(Process PsInfo)
+        {
+            if (cancelToken.IsCancellationRequested)
+            {
+                // キャンセルされたらTaskを終了する.
+               
+                return "Canceled";
+            }
+            output = PsInfo.StandardOutput.ReadLine();
+            output = output?.Replace("\r\r\n", "\n"); // 改行コードの修正
+            if (output != "") Invoke(output);
+
+            return  output;
+        }
+        public  void CreOutput(Process PsInfo)
+        {
+
+        }
+        public void Invoke(string output)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(this.UpdateText));
+            }
+            else
+            {
+            }
+
+        }
+        public string InvokeButton()
+        {
+            if (this.InvokeRequired)
+            {
+
+                    this.Invoke(new Action(this.ButtonUpdate));
+                return "ok";
+            }
+            else
+            {
+
+                    return "out!";
+           
+            }
+
+        }
+        public void ButtonUpdate()
+        {
+
+            button4.Text = "実行";
+            button4.Enabled = true;
+        }
+        public string InvokeButton2()
+        {
+            if (this.InvokeRequired)
+            {
+          
+                    this.Invoke(new Action(this.ButtonUpdate2));
+                    return "ok";
+           
+            }
+            else
+            {
+                return "out"; 
+            }
+
+        }
+        public void ButtonUpdate2()
+        {
+            //button4.Enabled = false;
+
+            button4.Enabled = false;
+            button4.Text = "終了中";
+        }
+        public void UpdateText()
+        {
+            textBox1.Text += output;
+            //カレット位置を末尾に移動
+            textBox1.SelectionStart = textBox1.Text.Length;
+            //テキストボックスにフォーカスを移動
+            textBox1.Focus();
+            //カレット位置までスクロール
+            textBox1.ScrollToCaret();
+        }
+        public  string Stop(Process PsInfo)
+        {
+     
+                tokenSource.Cancel();
+
+
+
+                PsInfo.Kill();
+                PsInfo.WaitForExit();
+                return "stop";
+         
+        }
+
+
+
+
+
+    }
+}
