7c24c39d2a817c27367a1d2069ba20ce4dcb1109
nishikawa-r <102705383+nishikawa-r@users.noreply.github.com>
2022-09-01 13:32:13 +0900

    無駄な処理の記述の削除を行った。

--- WindowsFormsApp2/ComandProcess.cs
+++ WindowsFormsApp2/ComandProcess.cs
@@ -13,37 +13,12 @@ namespace WindowsFormsApp2
 {
     public class ComandProcess
     {
-        private string Direct { get; set; }
         private string Arguments { get; set; }
-        private Process PsInfo { get; set; }
-        CancellationToken cancelToken;
-        private CancellationTokenSource TokenSource { get; set; }
-        CancellationTokenSource cancelTokensource;//キャンセル判定用
   
-        public async void Main(TextBox form)
-        {
-
-
-        }       
         public string CreateArguments(Controller controller)
         {
             Arguments = controller.Direct+" "+controller.Url+" "+controller.Username+" "+controller.Password;
             return Arguments;
         }
-        public void ClosePsInfoCommand()
-        {
-            if (cancelTokensource != null)
-            {
-                cancelTokensource.Cancel();//キャンセルを発行
-             
-            }
-
-
-        }
-        public void HeavyMethod(CancellationToken token, TextBox form1)
-        {
-            //キャンセル用トークン作成
-        }
     }
-    
 }
